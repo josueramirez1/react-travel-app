ec4a060aca56be25bb026da49af3e01d81f4e831
Josue Ramirez <josue67ramirez@gmail.com>
2025-05-21 11:33:11 -0700

    changing local API to Render

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
-const PORT = 8000;
-const {default: axios} = require("axios");
+const PORT = process.env.PORT || 8000;
+const { default: axios } = require("axios");
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
@@ -10,111 +10,108 @@ app.use(express.json());
 // Default
 
 app.get("/", (req, res) => {
-    res.status(200).json("Welcome to my traventure app");
+  res.status(200).json("Welcome to my traventure app");
 });
 
 // Get all posts
 app.get("/posts", async (req, res) => {
-    const url = `${process.env.ASTRA_URL}?page-size=20`;
+  const url = `${process.env.ASTRA_URL}?page-size=20`;
 
-    const options = {
-        method: "GET",
-        headers: {
-            "X-Cassandra-Token": process.env.TOKEN,
-        },
-    };
-    try {
-        const response = await axios(url, options);
-        res.status(200).json(response.data);
-
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({message: err});
-    }
+  const options = {
+    method: "GET",
+    headers: {
+      "X-Cassandra-Token": process.env.TOKEN,
+    },
+  };
+  try {
+    const response = await axios(url, options);
+    res.status(200).json(response.data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: err });
+  }
 });
 // Get a post
 app.get("/posts/:postId", async (req, res) => {
-    const id = req.params.postId;
-    const url = `${process.env.ASTRA_URL}/${id}`;
-    const options = {
-        method: "GET",
-        headers: {
-            "X-Cassandra-Token": process.env.TOKEN,
-        },
-    };
-    try {
-        const response = await axios(url, options);
-        res.status(200).json(response.data);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({message: err});
-    }
+  const id = req.params.postId;
+  const url = `${process.env.ASTRA_URL}/${id}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-Cassandra-Token": process.env.TOKEN,
+    },
+  };
+  try {
+    const response = await axios(url, options);
+    res.status(200).json(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err });
+  }
 });
 
 // Update post
 
 app.put("/edit/:postId", async (req, res) => {
-    const id = req.params.postId;
-    const data = req.body.data;
-    const url = `${process.env.ASTRA_URL}/${id}`;
-    const options = {
-        method: "PUT",
-        headers: {
-            Accepts: "application/json",
-            "X-Cassandra-Token": process.env.TOKEN,
-        },
-        data,
-    };
-    try {
-        const response = await axios(url, options);
-        res.status(200).json(response.data);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({message: err});
-    }
+  const id = req.params.postId;
+  const data = req.body.data;
+  const url = `${process.env.ASTRA_URL}/${id}`;
+  const options = {
+    method: "PUT",
+    headers: {
+      Accepts: "application/json",
+      "X-Cassandra-Token": process.env.TOKEN,
+    },
+    data,
+  };
+  try {
+    const response = await axios(url, options);
+    res.status(200).json(response.data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: err });
+  }
 });
 
 // Add a post
 app.post("/create", async (req, res) => {
-    const data = req.body.data
-    const url = process.env.ASTRA_URL;
+  const data = req.body.data;
+  const url = process.env.ASTRA_URL;
 
-    const options = {
-        method: "POST",
-        headers: {
-            Accept: "application/json",
-            "X-Cassandra-Token": process.env.TOKEN,
-        },
-        data: data,
-    }
-    try {
-        const response = await axios(url, options);
-        res.status(200).json(response.data);
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({message: error});
-    }
-})
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "X-Cassandra-Token": process.env.TOKEN,
+    },
+    data: data,
+  };
+  try {
+    const response = await axios(url, options);
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error });
+  }
+});
 
 // Delete Post
 app.delete("/delete/:postId", async (req, res) => {
-    const id = req.params.postId
-    const url = `${process.env.ASTRA_URL}/${id}`
+  const id = req.params.postId;
+  const url = `${process.env.ASTRA_URL}/${id}`;
 
-    const options = {
-        method: "DELETE",
-        headers: {
-            "X-Cassandra-Token": process.env.TOKEN,
-        }
-    }
-    try {
-        const response = await axios(url, options)
-        res.status(200).json(response.data);
-    } catch (err) {
-        console.error(err);
-    }
-
-
-})
+  const options = {
+    method: "DELETE",
+    headers: {
+      "X-Cassandra-Token": process.env.TOKEN,
+    },
+  };
+  try {
+    const response = await axios(url, options);
+    res.status(200).json(response.data);
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 app.listen(PORT, console.log("Server is running on PORT " + PORT));
